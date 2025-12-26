78cfb1f96b55f71b449eab94563dc97cc120605d
HaShiRaMaReiZo <zwemannhtet65@gmail.com>
2025-12-27 02:42:05 +0630

    Fix MongoDB connection options - remove unsupported bufferMaxEntries

--- src/config/database.js
+++ src/config/database.js
@@ -23,8 +23,6 @@ const connectDB = async () => {
     cachedConnection = mongoose.connect(process.env.MONGODB_URI, {
       serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
       socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
-      bufferCommands: false, // Disable mongoose buffering
-      bufferMaxEntries: 0, // Disable mongoose buffering
     });
 
     const conn = await cachedConnection;
